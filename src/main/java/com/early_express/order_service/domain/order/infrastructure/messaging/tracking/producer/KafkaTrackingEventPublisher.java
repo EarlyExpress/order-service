8200be640a8d1c6f506ff4bdabd9c39fd3efe4b6
jun <pickjog@gmail.com>
2025-11-27 18:04:57 +0900

    feat(event): 퍼블리서 추가

--- src/main/java/com/early_express/order_service/domain/order/infrastructure/messaging/tracking/producer/KafkaTrackingEventPublisher.java
+++ src/main/java/com/early_express/order_service/domain/order/infrastructure/messaging/tracking/producer/KafkaTrackingEventPublisher.java
@@ -19,7 +19,7 @@ public class KafkaTrackingEventPublisher implements TrackingEventPublisher {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    @Value("${spring.kafka.topic.tracking-start-requested-event}")
+    @Value("${spring.kafka.topic.tracking-start-requested}")
     private String trackingStartRequestedTopic;
 
     @Override
